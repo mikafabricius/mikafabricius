3599aba655edd4bdbe4c1cf3c255f0231b600961
Mika Fabricius <mikafabricius@hotmail.com>
2025-09-19 01:38:05 +0200

    Add files via upload

--- Graph.cs
+++ Graph.cs
@@ -2,193 +2,272 @@ using Godot;
 using System;
 using System.Linq;
 using System.IO;
-using System.Threading.Tasks;
 using System.Globalization;
 using System.Collections.Generic;
 
-
-// In-editor running
-//[Tool]
 public partial class Graph : Control
 {
-	public float[] xticks, yticks;
+	public float[] dataNox = new float[] { };
 	public string[] dataTime = new string[] { };
-	public float[] dataNox = new float[] { }; 
 	public static int count = 0;
 	private double time;
-	public Rect2 viewport;
 	public Font defaultfont = ThemeDB.FallbackFont;
-
 	public const int dataPointsMax = 36;
-	public float[] dataX = new float[dataPointsMax];
-	public float[] dataY = new float[dataPointsMax];
+	public List<Vector2> data = new List<Vector2>();
+	public List<Vector2> scaledPoints = new List<Vector2>();
+	private readonly List<string> visibleTimes = new List<string>();
+	public Vector2 origin = new Vector2(100, 600);
 	private const int Width = 800;
 	private const int Height = 500;
 	private const int Margin = 100;
-	public const int Ticks = 5;
-	public List<Vector2> data = new List<Vector2>();
-	public List<Vector2> scaledPoints = new List<Vector2>();
-	public Vector2 origin = new Vector2(Margin, Height + Margin);
+	private float axisYMin = float.NaN, axisYMax = float.NaN;
+	private float yHystPct = 0.10f;
+	private float yHystAbs = 0f;
+	private int yDesiredTicks = 5;
+	private readonly List<float> yTickValues = new List<float>();
 
-
-
-	
 	public override void _PhysicsProcess(double delta)
 	{
-		time += delta*(double) 1;
-		if (count < (int) time)
+		time += delta;
+		if (count < (int)time)
 		{
-			if (count > dataNox.Count())
-			{
-				GD.Print("no more data");
-			}
+			if (count > dataNox.Length - 1) return;
 			count++;
-			if (data.Count() < dataPointsMax - 1)
+			if (data.Count < dataPointsMax - 1)
 			{
-				data.Add(new Vector2((float)count * 5,dataNox[count]));
+				data.Add(new Vector2(count * 5, dataNox[count]));
+				visibleTimes.Add(count < dataTime.Length ? dataTime[count] : "");
 			}
 			else
 			{
 				data.RemoveAt(0);
-				data.Add(new Vector2((float)count * 5,dataNox[count]));
+				if (visibleTimes.Count > 0) visibleTimes.RemoveAt(0);
+				data.Add(new Vector2(count * 5, dataNox[count]));
+				visibleTimes.Add(count < dataTime.Length ? dataTime[count] : "");
 			}
 			Plot(data);
 		}
-
 	}
 
 	public override void _Ready()
 	{
-		GD.Print("hello");
-		//data_read_timer = GetNode<Timer>("Timer");
-		//data_read_timer.Timeout += () => UpdatePlot();
 		ReadData();
-		//data_read_timer.Connect(Timer.SignalName.Timeout, Callable.From(StreamPlot));
-		
 	}
-
 
 	public override void _Draw()
 	{
-
-		DrawLine(origin, origin + new Vector2(Width, 0), Colors.Gray, 2);
-		DrawLine(origin, origin - new Vector2(0, Height), Colors.Gray, 2);
-
-		// TODO: Make the ticks follow the actual datapoints, so they slide
-		// off the axis like a waterfall. Keep the distance, which means max
-		// 4 ticks on the screen. Same goes for the yticks.
-		/*for (int i = 1; i < Ticks; i++)
+		// TODO make envelope with the min and max from the miljøstyrelsen
+		int fontSize = 14;
+		Color labelColor = Colors.LightGray;
+		Color axisColor = Colors.Gray;
+		Color tickColor = Colors.Gray;
+		DrawLine(origin, origin + new Vector2(Width, 0), axisColor, 2);
+		DrawLine(origin, origin - new Vector2(0, Height), axisColor, 2);
+		if (scaledPoints.Count > 0)
 		{
-			float xTick = i / (float)(Ticks - 1) * Width;
-			float yTick = i / (float)(Ticks - 1) * Height;
-
-			DrawLine(origin + new Vector2(xTick,-5), origin + new Vector2(xTick,5), Colors.Gray);
-			DrawLine(origin + new Vector2(-5,-yTick), origin + new Vector2(+5,-yTick), Colors.Gray);
-		}
-		*/
-
-		for (int i = 0; i < scaledPoints.Count()-1; i++)
-		{
-			if (i % Ticks == 0)
+			var tickIdx = GetDistributedIndices(scaledPoints.Count, 4);
+			foreach (int i in tickIdx)
 			{
-				DrawLine(new Vector2(scaledPoints[i].X, -5 + Height), new Vector2(scaledPoints[i].X, 5 + Height), Colors.Gray);
-				DrawLine(new Vector2(Margin, Margin) + new Vector2(-5, scaledPoints[i].Y), new Vector2(Margin,Margin) + new Vector2(5, scaledPoints[i].Y), Colors.Gray);
-
+				float xScreen = Margin + scaledPoints[i].X;
+				DrawLine(new Vector2(xScreen, origin.Y - 5), new Vector2(xScreen, origin.Y + 5), tickColor);
+				string xLabel = FormatTimeLabel(SafeGet(visibleTimes, i));
+				if (string.IsNullOrEmpty(xLabel)) xLabel = SafeGet(data, i).X.ToString("0");
+				Vector2 xSize = defaultfont.GetStringSize(text: xLabel, fontSize: fontSize);
+				Vector2 xPos = new Vector2(xScreen - xSize.X / 2f, origin.Y + 20f);
+				DrawString(defaultfont, xPos, xLabel, HorizontalAlignment.Left, -1, fontSize, labelColor);
 			}
-			//GD.Print(scaledPoints[i]);
-			//GD.Print(scaledPoints[i+1]);
-			DrawLine(new Vector2(Margin, Margin) + scaledPoints[i],new Vector2(Margin, Margin) + scaledPoints[i+1], Colors.Green, 2, true);
 		}
+
+		if (yTickValues.Count > 0 && !float.IsNaN(axisYMin) && !float.IsNaN(axisYMax) && axisYMax > axisYMin)
+		{
+			foreach (var tickVal in yTickValues)
+			{
+				float yScreen = YValueToScreen(tickVal);
+				DrawLine(new Vector2(Margin - 5, yScreen), new Vector2(Margin + 5, yScreen), tickColor);
+				string yLabel = tickVal.ToString("0.###", CultureInfo.InvariantCulture);
+				Vector2 ySize = defaultfont.GetStringSize(text: yLabel, fontSize: fontSize);
+				Vector2 yPos = new Vector2(Margin - 10f - ySize.X, yScreen + ySize.Y * 0.35f);
+				DrawString(defaultfont, yPos, yLabel, HorizontalAlignment.Left, -1, fontSize, labelColor);
+			}
+		}
+
+		for (int i = 0; i < scaledPoints.Count - 1; i++)
+			DrawLine(new Vector2(Margin, Margin) + scaledPoints[i], new Vector2(Margin, Margin) + scaledPoints[i + 1], Colors.Green, 2, true);
+
+		Vector2 xLabelSize = defaultfont.GetStringSize("Time [HH.MM.SS]");
+		Vector2 xLabelPos = new Vector2(origin.X + Width/2 - xLabelSize.X/2, origin.Y + Margin/2);
+		DrawString(defaultfont, xLabelPos, "Time [HH.MM.SS]", HorizontalAlignment.Left, -1, fontSize, labelColor);
+		Vector2 yLabelSize = defaultfont.GetStringSize("nox emmision [mg/Nm3]", fontSize: fontSize);
+		Vector2 yLabelPos = new Vector2(origin.X - Margin/2, origin.Y - Height/2 + yLabelSize.X/2);
+		DrawSetTransformMatrix(new Transform2D(Mathf.DegToRad(-90), yLabelPos));
+		DrawString(defaultfont, Vector2.Zero, "nox emmision [mg/nm3]", HorizontalAlignment.Left, -1, fontSize, labelColor);
 	}
 
 	public void ReadData()
 	{
-		using(StreamReader reader = new StreamReader("data/complete_measurements_with_designations_20240520_000000_20240603_000000.txt"))
+		using (StreamReader reader = new StreamReader("data/complete_measurements_with_designations_20240520_000000_20240603_000000.txt"))
 		{
 			bool firstLine = false;
 			while (!reader.EndOfStream)
 			{
 				var line = reader.ReadLine();
 				var values = line.Split(',');
-				if (!firstLine)
+				if (!firstLine) firstLine = true;
+				else
 				{
-					firstLine = true;
-				}
-				else 
-				{
-					dataTime = dataTime.Append<string>((string)values[0]).ToArray();
-					dataNox = dataNox.Append<float>(float.Parse(values[5], CultureInfo.InvariantCulture.NumberFormat)).ToArray();
+					dataTime = dataTime.Append(values[0]).ToArray();
+					dataNox = dataNox.Append(float.Parse(values[5], CultureInfo.InvariantCulture.NumberFormat)).ToArray();
 				}
 			}
 		}
 	}
 
-
 	public void Plot(List<Vector2> data)
 	{
-		float xMax = float.MinValue, xMin = float.MaxValue;
-		float yMax = float.MinValue, yMin = float.MaxValue;
-
+		float xMax = float.MinValue, xMin = float.MaxValue, yDataMax = float.MinValue, yDataMin = float.MaxValue;
 		foreach (var point in data)
 		{
 			xMax = Math.Max(xMax, point.X);
 			xMin = Math.Min(xMin, point.X);
-			yMax = Math.Max(yMax, point.Y);
-			yMin = Math.Min(yMin, point.Y);
+			yDataMax = Math.Max(yDataMax, point.Y);
+			yDataMin = Math.Min(yDataMin, point.Y);
 		}
-
 		float xRange = xMax - xMin == 0 ? 1 : xMax - xMin;
-		float yRange = yMax - yMin == 0 ? 1 : yMax - yMin;
-
+		if (UpdateYAxisRange(yDataMin, yDataMax)) RebuildYTicks();
+		if (float.IsNaN(axisYMin) || float.IsNaN(axisYMax) || axisYMax <= axisYMin)
+		{
+			axisYMin = yDataMin;
+			axisYMax = yDataMax;
+			if (axisYMax - axisYMin < 1e-6f) { axisYMin -= 0.5f; axisYMax += 0.5f; }
+			RebuildYTicks();
+		}
+		float yAxisRange = axisYMax - axisYMin;
+		if (yAxisRange < 1e-6f) yAxisRange = 1.0f;
 		scaledPoints.Clear();
 		foreach (var point in data)
 		{
 			float scaledX = ((point.X - xMin) / xRange) * Width;
-			float scaledY = Height - ((point.Y - yMin) / yRange) * Height;
-			if (scaledY == 0)
-			{
-				scaledY = Height * 0.05f;
-			}
-			else if (scaledY == Height)
-			{
-				scaledY = Height * 0.95f;
-			}
-
-			if (scaledPoints.Count() < dataPointsMax - 1)
-			{
-				scaledPoints.Add(new Vector2(scaledX,scaledY));
-			}
-			else 
-			{
-				scaledPoints.RemoveAt(0);
-				scaledPoints.Add(new Vector2(scaledX,scaledY));
-			}
+			float scaledY = Height - ((point.Y - axisYMin) / yAxisRange) * Height;
+			scaledY = Math.Max(0, Math.Min(Height, scaledY));
+			if (scaledY == 0) scaledY = Height * 0.05f;
+			else if (scaledY == Height) scaledY = Height * 0.95f;
+			if (scaledPoints.Count < dataPointsMax - 1) scaledPoints.Add(new Vector2(scaledX, scaledY));
+			else { scaledPoints.RemoveAt(0); scaledPoints.Add(new Vector2(scaledX, scaledY)); }
 		}
 		QueueRedraw();
 	}
 
-	public void FIFO(float[] array, int arraySize, float newValue)
+	private bool UpdateYAxisRange(float yDataMin, float yDataMax)
 	{
-
-		float tmp1 = 0.0f;
-		float tmp2 = 0.0f;
-		for (int i = arraySize - 1; i > 0; i = i - 1)
+		if (float.IsNaN(axisYMin) || float.IsNaN(axisYMax))
 		{
-			if (i == arraySize - 1)
-			{
-				tmp1 = array[i-1];
-				array[i-1] = array[i];
-			}
-			else 
-			{
-				tmp2 = array[i-1];
-				array[i-1] = tmp1;
-				tmp1 = tmp2;
-			}
+			axisYMin = yDataMin;
+			axisYMax = yDataMax;
+			if (axisYMax - axisYMin < 1e-6f) { axisYMin -= 0.5f; axisYMax += 0.5f; }
+			return true;
 		}
+		float currentRange = Math.Max(1e-6f, axisYMax - axisYMin);
+		float hysteresis = Math.Max(yHystAbs, yHystPct * currentRange);
+		bool changed = false;
+		if (yDataMin < axisYMin - hysteresis) { axisYMin = yDataMin; changed = true; }
+		else if (yDataMin > axisYMin + hysteresis) { axisYMin = yDataMin; changed = true; }
+		if (yDataMax > axisYMax + hysteresis) { axisYMax = yDataMax; changed = true; }
+		else if (yDataMax < axisYMax - hysteresis) { axisYMax = yDataMax; changed = true; }
+		if (changed && axisYMax - axisYMin < 1e-6f) { axisYMin -= 0.5f; axisYMax += 0.5f; }
+		return changed;
+	}
 
-		array[arraySize-1]  = newValue;
+	private void RebuildYTicks()
+	{
+		yTickValues.Clear();
+		float min = axisYMin, max = axisYMax;
+		if (max <= min) max = min + 1f;
+		ComputeNiceAxis(min, max, yDesiredTicks, out float niceMin, out float niceMax, out float tickStep, out int tickCount);
+		axisYMin = niceMin;
+		axisYMax = niceMax;
+		for (int i = 0; i < tickCount; i++)
+			yTickValues.Add((float)Math.Round(niceMin + i * tickStep, 6, MidpointRounding.AwayFromZero));
+	}
 
-		return;
+	private static float NiceNum(float range, bool round)
+	{
+		double exponent = Math.Floor(Math.Log10(Math.Max(1e-12, range)));
+		double fraction = range / Math.Pow(10, exponent);
+		double niceFraction;
+		if (round)
+		{
+			if (fraction < 1.5) niceFraction = 1;
+			else if (fraction < 3) niceFraction = 2;
+			else if (fraction < 7) niceFraction = 5;
+			else niceFraction = 10;
+		}
+		else
+		{
+			if (fraction <= 1) niceFraction = 1;
+			else if (fraction <= 2) niceFraction = 2;
+			else if (fraction <= 5) niceFraction = 5;
+			else niceFraction = 10;
+		}
+		return (float)(niceFraction * Math.Pow(10, exponent));
+	}
+
+	private static void ComputeNiceAxis(float min, float max, int targetTicks, out float niceMin, out float niceMax, out float tickSpacing, out int tickCount)
+	{
+		float rawRange = Math.Max(1e-6f, max - min);
+		float niceRange = NiceNum(rawRange, false);
+		tickSpacing = NiceNum(niceRange / Math.Max(2, targetTicks - 1), true);
+		niceMin = (float)Math.Floor(min / tickSpacing) * tickSpacing;
+		niceMax = (float)Math.Ceiling(max / tickSpacing) * tickSpacing;
+		tickCount = (int)Math.Round((niceMax - niceMin) / tickSpacing) + 1;
+		if (tickCount > targetTicks + 2)
+		{
+			float span = niceMax - niceMin;
+			tickSpacing = NiceNum(span / targetTicks, true);
+			niceMin = (float)Math.Floor(min / tickSpacing) * tickSpacing;
+			niceMax = (float)Math.Ceiling(max / tickSpacing) * tickSpacing;
+			tickCount = (int)Math.Round((niceMax - niceMin) / tickSpacing) + 1;
+		}
+		if (tickCount < 2)
+		{
+			tickCount = 2;
+			tickSpacing = Math.Max(1e-3f, (niceMax - niceMin) / (tickCount - 1));
+		}
+	}
+
+	private float YValueToScreen(float yVal)
+	{
+		float range = Math.Max(1e-6f, axisYMax - axisYMin);
+		float t = (yVal - axisYMin) / range;
+		float yPlot = Height - t * Height;
+		yPlot = Math.Max(0, Math.Min(Height, yPlot));
+		return Margin + yPlot;
+	}
+
+	private static List<int> GetDistributedIndices(int count, int maxTicks)
+	{
+		var indices = new List<int>();
+		if (count <= 0) return indices;
+		int ticks = Math.Min(maxTicks, count);
+		if (ticks == 1) { indices.Add(count - 1); return indices; }
+		for (int i = 0; i < ticks; i++)
+		{
+			int idx = (int)Math.Round(i * (count - 1) / (float)(ticks - 1));
+			if (indices.Count == 0 || idx != indices[^1]) indices.Add(idx);
+		}
+		return indices;
+	}
+
+	private static string FormatTimeLabel(string raw)
+	{
+		if (string.IsNullOrWhiteSpace(raw)) return string.Empty;
+		if (DateTime.TryParse(raw, CultureInfo.InvariantCulture, DateTimeStyles.AssumeLocal, out var dt))
+			return dt.ToString("HH:mm:ss");
+		return raw;
+	}
+
+	private static T SafeGet<T>(IList<T> list, int idx)
+	{
+		if (list == null || idx < 0 || idx >= list.Count) return default!;
+		return list[idx];
 	}
 }
